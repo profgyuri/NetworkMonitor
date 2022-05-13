f6615c742d0930af04cbb04521d710646160d0f4
Turbék György <gyorgy.turbek@gmail.com>
2022-05-13 15:55:06 +0200

    Modified the startup position.

--- NetworkMonitor.Lib/GlobalUsings.cs
+++ NetworkMonitor.Lib/GlobalUsings.cs
@@ -1,5 +1,1 @@
 ﻿#pragma warning disable IDE0065
-global using System.Collections.Generic;
-global using System.Collections.ObjectModel;
-global using System.Linq;
-global using System.Threading.Tasks;
--- NetworkMonitor.WPF/MainWindow.xaml.cs
+++ NetworkMonitor.WPF/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
 ﻿namespace NetworkMonitor.WPF;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
@@ -22,5 +9,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        //Place the window to the bottom right of the screen
+        Left = SystemParameters.PrimaryScreenWidth - Width - 20;
+        Top = SystemParameters.PrimaryScreenHeight - Height - 70;
     }
 }
